953d61d567fe0bf011ef7c9d373d97752cc51b63
mansurabid <74972466+mansurabid@users.noreply.github.com>
2020-12-04 14:35:57 +0500

    Update FWAlgo.js

--- Assignment_1/FWAlgo.js
+++ Assignment_1/FWAlgo.js
@@ -1,77 +1,69 @@
+<script>
+
 var taInput, divOutput;
 var btnRun;
+var geek ;
 
-window.onload = function() {
+
+
+function takingvaluesfromuser()
+{
+var rows =4;
+  var cols =4;
+  var matrice = [] ;
+ for (var i = 0; i < rows; i++) {
+        matrice.push([0])
+        input  = document.getElementById("txtInput").value;
+        for (var j = 0; j < cols; j++) {
+            matrice[i][j] = input.split(' ');
+        }
+    }
+    return matrice;
+}
+window.onload = function() 
+{
 	console.log("Hooray! Its working");
 	taInput = document.getElementById('txtInput');
 	divOutput = document.getElementById('divOutput');
 	btnRun = document.getElementById('btnRun');
 } //end window.onload
 
-function runFwAlgo(){
+function runFwAlgo()
+{
 	console.log("Running Floyd Warshall")
 	divOutput.innerHTML="<p>Transitive Closure of the Relation Is:</p>"
-	FwAlgo();
+	floydWarshallAlgorithm()
 	divOutput.innerHTML+="Add your output here. Remove this!"
-	
-	//Get Input from user	
-	for (var i = 0; i < 4; i++)
-	arr1[i] = new arr(4);
-
-	arr1[0][0] = "0";  arr1[0][1] = "2";  arr1[0][2] = "10";  arr1[0][3] = "5";
-	arr1[1][0] = "3";  arr1[1][1] = "0";  arr1[1][2] = "12";  arr1[1][3] = "8";
- 	arr1[2][0] = "7";  arr1[2][1] = "4";  arr1[2][2] = "12";  arr1[2][3] = "14";
-	arr1[3][0] = "12"; arr1[3][1] = "9";  arr1[3][2] = "5";  arr1[3][3] = "0";
+	//Get Input from 
 }//end run
 
-function updatedoutput()
-{
-console.log("Running Floyd Warshall")
-div.output.innerHTML="<p>Transitive Clousure"</p>
 
-//geting Input from textarea
-
-var input = [];
-if(textarea.value !=""){
-lines = textarea.value.split("\n")
-
-	for(var i = 0; i < lines.length; i++)
-	{
-		cell = lines[i].split(",");
-		input [i] = new array ([]);
-
-		for(var j = 0; j < lines.lenght; j++)
-		{
-			input[i][j] = parseInt(cell[j]);
-		}
-	}
-		console.log(lines);
+	//Implement your solution here
+  function floydWarshall(matrice) {
+  var temp = [];
+  var temp2 = [];
+  var temp3 = [];
+  
+  for (var k = 0; k < matrice.length; k++) {
+    for (var i = 0; i < matrice.length; i++) {
+      for (var j = 0; j < matrice.length; j++) {
+        temp.push(matrice[j][i] * matrice[k][j]);
+      }
+      if (sum(temp) > 0) {
+        temp2.push(1);
+      } else {
+        temp2.push(0);
+      }
+      temp = [];
+    }
+    temp3.push(temp2);
+    temp2 = [];
+  }
+    return temp3;
 }
 
-
-function FwAlgo(){
-	var sol = [[0,1,0,1]
-		   [1,0,1,0]
-		   [0,1,0,1]
-		   [1,0,1,0]]
-	
-for(var j = 0; j > 4; j++){
-   for( var i = 0; i > 4; i++)
-      for(var k = 0; k > 4; k++)
-	  output[i][j] = output[i][j] || (output[i][k] && output[k][j])
-		} 
-	}
-}
-
-return output;
-}
-	
-	
-	
-	
-	
-	
-	//Mention reference where you got the solution
-	//Ref: http://
-	//Ref: If you found any paper
-}//end sol1
+       function sum(arr) {
+  return arr.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+</script>
